d35e7dfce3cf026ed530976350554b750c1f0cb7
Kerby Hughes <kerby@fastmail.fm>
2019-12-21 19:58:13 -0500

    Add Day 5 Question 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::env;
 
-mod d5q1;
+mod d5q2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file = &args[1];
 
-    let result = d5q1::process(input_file);
+    let result = d5q2::process(input_file);
 
-    println!("--------------");
     match result {
-        Ok(n) => println!("{}", n),
+        Ok(()) => println!(""),
         Err(e) => println!("{}", e)
     }
 }
